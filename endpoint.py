73719b7fa74fa6d2a1a74e7cfe875091c46b158e
EUING <coke1324@naver.com>
2021-06-24 10:04:28 +0000

    endpoint 수정

--- endpoint.py
+++ endpoint.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/ping", methods=["GET"])
-def ping():
-    return "pong"
+@app.route("/files", methods=["POST"])
+def add_file():
+    add_file = request.json
+    return jsonify(add_file), 201
+
+@app.route("/files/<string:file_name>", methods=["PATCH"])
+def modify_file(file_name):
+    modify_file = request.json
+    print(modify_file)
+    return jsonify(modify_file), 200
+
+@app.route("/files/<string:file_name>", methods=["DELETE"])
+def remove_file(file_name):
+    print(file_name)
+    return "", 202
+
+@app.route("/files/<string:file_name>/name", methods=["PATCH"])
+def rename_file(file_name):
+    rename_file = request.json
+    print(rename_file)
+    return jsonify(rename_file), 200
 
 app.run(host="0.0.0.0", port=56380)
